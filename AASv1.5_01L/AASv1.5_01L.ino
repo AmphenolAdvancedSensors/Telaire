cdfb333dbcf60db77a670967ad2db481b55cc846
neilsroberts <neil.s.roberts@amphenol-sensors.com>
2017-12-22 12:30:11 +0000

    Add files via upload

---
+++ AASv1.5_01L/AASv1.5_01L.ino
@@ -0,0 +1,163 @@
+unsigned int displayrate = 1000; //delay between display or printed values in mS
+
+//__________________________________________________________________________________
+
+unsigned long sampletime;       // variable for timer
+unsigned char ide_workaround = 0;
+
+#include <SoftwareSerial.h>
+#define ENABLE_LCD
+#define MAX_RANGE 3000
+
+
+//set up LCD
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_MOSI   11 //SDA
+#define OLED_CLK   12 //SCL
+#define OLED_DC    9 // D/C
+#define OLED_CS     11// not used
+#define OLED_RESET 10 //RESET
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+
+//set up laser sensor
+#define SENSOR_BAUD_RATE 9600
+#define DEBUG_BARUD_RATE 19200
+
+#define _DBG_RXPIN_ 6
+#define _DBG_TXPIN_ 5
+SoftwareSerial mySerial(_DBG_RXPIN_, _DBG_TXPIN_);
+
+byte buffer3[32];
+static unsigned int data[3] = {0};
+byte MSB, LSB;
+
+byte READ_VAL_CMD[8] = {0x20, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x17, 0x7A};
+
+void setup() {
+  Serial.begin(19200);
+  mySerial.begin(SENSOR_BAUD_RATE);     // opens serial port, sets data rate to 9600 bps
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC);
+  // Clear the buffer.
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.print("AASv1.5_01L");
+  display.display();
+  delay(2000);
+  displayTitle();
+  display.display();
+
+  Serial.println(F("Amphenol Advanced Sensors AASv1.5_01L"));
+  Serial.print(F("PM2.5 µg/m3"));
+
+}
+
+void loop() {
+    if (millis() >= (displayrate + sampletime))
+  {
+    sampletime = millis();  //resets timer
+  int i;
+  static int j = 0;
+  uint16_t crc = 0;
+
+  if ( readPM() )
+    //DebugSerial.println(mySerial.available());
+  {
+    mySerial.readBytes(buffer3, 14);
+    crc = CRC16(buffer3, 12);
+
+    if (crc == (buffer3[12] << 8 | buffer3[13] ))
+    {
+      //data[0] = buffer3[6] * 255 + buffer3[7];  //PM1.0
+      data[1] = buffer3[8] * 255 + buffer3[9];  //PM2.5
+      //data[2] = buffer3[10] * 255 + buffer3[11]; //PM10
+      if ( data[1] > MAX_RANGE ) {
+        data[1] = MAX_RANGE;
+      }
+    }
+  }
+
+  Serial.println(data[1]);
+
+  displayReading();
+
+  }
+}
+
+void displayTitle()
+{
+  display.clearDisplay();
+  display.setCursor(15, 55);
+  display.setTextSize(1);
+  display.print("Laser Dust Reading");
+  //display.display();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.print(" Advanced Sensors");
+
+}
+
+void displayReading() {
+  //display.clearDisplay();
+  displayTitle();
+  display.setTextSize(3);
+  display.setCursor(5, 16);
+
+  //display.print(data[1]);
+  display.setCursor(30, 18);
+  display.print(data[1]);
+
+  display.setTextSize(1);
+  display.setCursor(83, 19);
+  display.print("ug/m3");
+
+  display.display();
+}
+
+/*void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
+  {
+  asm volatile ("  jmp 0");
+  }*/
+
+boolean readPM()
+{
+  mySerial.write(READ_VAL_CMD, 8);
+  delay(10);
+  //while ( !mySerial.available() ) {}
+  return true;
+
+}
+
+
+uint16_t CRC16 (uint8_t *puchMsg, uint8_t u8_Length ) /* The function returns the CRC as a unsigned short type */
+{
+  uint16_t u16_Reg_CRC = 0xFFFF;
+  uint8_t i, j = 0;
+  uint8_t u8_Temp_Reg = 0;
+  for (i = 0; i < u8_Length; i++)
+  {
+    u16_Reg_CRC ^= *puchMsg++;
+    for (j = 0; j < 8; j++)
+    {
+      if (u16_Reg_CRC & 0x0001)
+      {
+        u16_Reg_CRC = u16_Reg_CRC >> 1 ^ 0xA001;
+      }
+      else
+      {
+        u16_Reg_CRC >>= 1;
+      }
+    }
+  }
+  u8_Temp_Reg = u16_Reg_CRC >> 8;
+  return (u16_Reg_CRC << 8 | u8_Temp_Reg );
+}
+
---
+++ AASv1.5_01L_T67xx/AASv1.5_01L_T67xx.ino
@@ -0,0 +1,198 @@
+unsigned int displayrate = 1000; //delay between display or printed values in mS
+
+//__________________________________________________________________________________
+
+unsigned long sampletime;       // variable for timer
+
+#include "Wire.h"
+
+unsigned char ide_workaround = 0;
+
+#include <SoftwareSerial.h>
+
+#define MAX_RANGE 3000
+
+
+#define ADDR_6700  0x15 // default I2C slave address
+
+int CO2ppmValue;
+
+
+
+//set up LCD
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_MOSI   11 //SDA
+#define OLED_CLK   12 //SCL
+#define OLED_DC    9 // D/C
+#define OLED_CS     11// not used
+#define OLED_RESET 10 //RESET
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+SoftwareSerial mySerial(6, 5);
+
+byte buffer3[32];
+static unsigned int data[3] = {0};
+byte MSB, LSB;
+
+
+void setup() { //______________________________________SETUP LOOP___________________________
+  Serial.begin(19200);      //opens serial port
+  Wire.begin();
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC);
+  // Clear the buffer.
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.print("AASv1.5_01L_T67xx");
+  display.display();
+  delay(2000);
+
+  Serial.println("Amphenol Advanced Sensors AASv1.5_01L T67xx");
+  Serial.print("PM2.5 "); Serial.print("µg/m3,"); Serial.println(" CO2 ppm");
+  mySerial.begin(9600);     // opens sensor serial port, sets data rate to 9600 bps
+}
+
+void loop() {  //___________________________________LOOP_________________________________
+  if (millis() >= (displayrate + sampletime))
+  {
+    sampletime = millis();  //resets timer
+    int i;
+    static int j = 0;
+    uint16_t crc = 0;
+
+
+    if ( readPM() )
+    {
+      mySerial.readBytes(buffer3, 14);
+      crc = CRC16(buffer3, 12);
+
+      if (crc == (buffer3[12] << 8 | buffer3[13] ))
+      {
+        //data[0] = buffer3[6] * 255 + buffer3[7];  //PM1.0
+        data[1] = buffer3[8] * 255 + buffer3[9];  //PM2.5
+        //data[2] = buffer3[10] * 255 + buffer3[11]; //PM10
+        if ( data[1] > MAX_RANGE ) {
+          data[1] = MAX_RANGE;
+        }
+      }
+    }
+
+    GetCO2PPM();
+    /*
+      Serial.print(data[0]);
+      Serial.print(", ");
+      Serial.print(data[1]);
+      Serial.print(", ");  */
+    Serial.print(data[1]);
+    Serial.print(", ");
+    Serial.println(CO2ppmValue);
+
+
+    displayReading();
+
+  }
+}
+//___________________________________________SUB ROUTINES__________________________________
+
+
+
+void displayReading() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.print("  Amphenol Sensors");
+  display.setCursor(12, 57);
+
+  display.setCursor(12, 12);
+  display.print("PM2.5        CO2");
+
+  display.setTextSize(1);
+
+  display.setCursor(15, 46);
+  display.setTextSize(1);
+  display.print("ug/m3       ppm");
+  display.setCursor(14, 57);
+  display.print("Laser Dust  & CO2");
+
+  display.setTextSize(2);
+  display.setCursor(15, 25);
+  display.print(data[1]);
+
+  display.setCursor(75, 25);
+  display.print(CO2ppmValue);
+
+  display.display();
+}
+
+
+/*void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
+  {
+  asm volatile ("  jmp 0");
+  }*/
+
+boolean readPM()
+{
+
+  byte READ_VAL_CMD[8] = {0x20, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x17, 0x7A};
+
+  mySerial.write(READ_VAL_CMD, 8);
+  delay(10);
+  //while ( !mySerial.available() ) {}
+  return true;
+
+}
+
+
+uint16_t CRC16 (uint8_t *puchMsg, uint8_t u8_Length ) /* The function returns the CRC as a unsigned short type */
+{
+  uint16_t u16_Reg_CRC = 0xFFFF;
+  uint8_t i, j = 0;
+  uint8_t u8_Temp_Reg = 0;
+  for (i = 0; i < u8_Length; i++)
+  {
+    u16_Reg_CRC ^= *puchMsg++;
+    for (j = 0; j < 8; j++)
+    {
+      if (u16_Reg_CRC & 0x0001)
+      {
+        u16_Reg_CRC = u16_Reg_CRC >> 1 ^ 0xA001;
+      }
+      else
+      {
+        u16_Reg_CRC >>= 1;
+      }
+    }
+  }
+  u8_Temp_Reg = u16_Reg_CRC >> 8;
+  return (u16_Reg_CRC << 8 | u8_Temp_Reg );
+}
+
+
+
+void GetCO2PPM()
+{
+  byte CO2data[6];
+  // start I2C
+  Wire.beginTransmission(ADDR_6700);
+  Wire.write(0x04); Wire.write(0x13); Wire.write(0x8B); Wire.write(0x00); Wire.write(0x01);
+  // end transmission
+  Wire.endTransmission();
+  // read report of current gas measurement in ppm
+  Wire.requestFrom(ADDR_6700, 4);    // request 4 bytes from slave device
+  CO2data[0] = Wire.read();
+  CO2data[1] = Wire.read();
+  CO2data[2] = Wire.read();
+  CO2data[3] = Wire.read();
+  //  Serial.print("MSB: ");  Serial.print(CO2data[2]); Serial.print("  ");
+  //  Serial.print("LSB: ");  Serial.print(CO2data[3]); Serial.print("  ");
+  CO2ppmValue = ((CO2data[2] & 0x3F ) << 8) | CO2data[3];
+  if (CO2ppmValue = 16383) {
+    CO2ppmValue = 0;
+  }
+}
